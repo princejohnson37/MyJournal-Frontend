697182d0f95d4ee5d4bb06b37ccf0dd0453e7872
princejohnson37 <princejohnson37@gmail.com>
2023-09-20 20:38:38 +0530

    git changes resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import AuthPage from "./layouts/AuthPage/AuthPage";
 import LoginPage from "./pages/AuthPages/LoginPage";
 
 function App() {
   return (
     <>
-      <AuthPage>
-        <LoginPage/>
-      </AuthPage>
+      <LoginPage />
     </>
   );
 }
